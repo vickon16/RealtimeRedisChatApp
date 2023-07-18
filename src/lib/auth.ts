998adfd142113e1fb33cf7997f3f33e07c3a8c76
Nkachukwu Victor <nkachukwuvictor2016@gmail.com>
2023-07-18 12:03:00 +0100

    New functions

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -15,8 +15,8 @@ export const authOptions: NextAuthOptions = {
   },
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGlE_CLIENT_ID,
-      clientSecret: process.env.GOOGlE_CLIENT_SECRET,
+      clientId: process.env.GOOGlE_CLIENT_ID!,
+      clientSecret: process.env.GOOGlE_CLIENT_SECRET!,
     }),
   ],
   callbacks: {
